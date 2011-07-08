c4c5c9e42f4293b80767db1cc83769c877d85531
Oona <okraisan@cs.helsinki.fi>
2011-07-08 13:14:43 +0300

    license text

--- bits.c
+++ bits.c
@@ -16,13 +16,6 @@
  *
  * Page, section and figure numbers refer to IEC 62106, Edition 2
  *
- *
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
  */
 
 #include <stdio.h>
--- downmix.c
+++ downmix.c
@@ -5,13 +5,6 @@
  *
  * Creates heterodynes of the RDS signal; result needs to be downpass filtered
  *
- *
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
  * */
 
 #include <stdio.h>
