5c6ba0ed06e63471af14d433a3e9dcb5c4228d1f
Oona Räisänen <oona@kapsi.fi>
2016-04-22 16:30:11 +0300

    data blocks to data.cc

---
+++ src/data.h
@@ -0,0 +1,15 @@
+#ifndef DATA_H_
+#define DATA_H_
+
+#include <string>
+
+namespace redsea {
+
+std::string getLCDchar(int code);
+std::string getPTYname(int pty);
+std::string getCountryString(uint16_t pi, uint16_t ecc);
+std::string getLanguageString(uint16_t code);
+std::string getAppName(uint16_t aid);
+
+} // namespace redsea
+#endif // DATA_H_
