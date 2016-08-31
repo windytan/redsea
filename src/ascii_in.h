063d1abb659735aacf92ee3a80d35ac47efad86e
Oona Räisänen <oona@kapsi.fi>
2016-08-31 20:31:05 +0300

    reorganize

---
+++ src/ascii_in.h
@@ -0,0 +1,24 @@
+#ifndef ASCII_IN_H_
+#define ASCII_IN_H_
+
+#include <vector>
+
+namespace redsea {
+
+class AsciiBits {
+
+  public:
+    AsciiBits();
+    ~AsciiBits();
+    int getNextBit();
+    bool isEOF() const;
+
+  private:
+    bool is_eof_;
+
+};
+
+std::vector<uint16_t> getNextGroupRSpy();
+
+} // namespace redsea
+#endif // ASCII_IN_H_
