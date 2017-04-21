7f5695473d5bda2d5af50ed9ae82b30a3cec1324
andimik <andimik@yahoo.de>
2017-04-22 01:54:54 +0200

    Update rtl-rx_rdsspy.sh
    bug, hour <10 caused writing 2 files

--- rtl-rx_rdsspy.sh
+++ rtl-rx_rdsspy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rtl_fm -M fm -l 0 -A std -p 0 -s 171k -g 40 -F 9 $@ | redsea -x | tee `head -12l | tail -1l | cut -d" " -f1`_`date +%F`_`date +%k`-`date +%M`-`date +%S`_$2Hz.spy
+rtl_fm -M fm -l 0 -A std -p 0 -s 171k -g 40 -F 9 $@ | redsea -x | tee `head -12l | tail -1l | cut -d" " -f1`_`date +%F`_`date +%H`-`date +%M`-`date +%S`_$2Hz.spy
